0c7b37fec8b4ccc55bbe7cb1cd9131e835c08003
Samuel Bradley <samuel@samuel-bradley.com>
2024-12-04 00:08:45 +0000

    Add brains and blasts this turn to decision-relevant game state

--- src/main/java/game/DecisionRelevantGameState.java
+++ src/main/java/game/DecisionRelevantGameState.java
@@ -4,10 +4,10 @@ import dice.Die;
 
 import java.util.Arrays;
 
-public record DecisionRelevantGameState(Die[] diceInCup, Die[] diceOnTable) {
+public record DecisionRelevantGameState(Die[] diceInCup, Die[] diceOnTable, int blastsThisTurn, int brainsThisTurn) {
 
     public static DecisionRelevantGameState fromGameState(GameState gameState) {
-        return new DecisionRelevantGameState(gameState.diceInCup(), gameState.diceOnTable());
+        return new DecisionRelevantGameState(gameState.diceInCup(), gameState.diceOnTable(), gameState.blastsThisTurn(), gameState.brainsThisTurn());
     }
 
     @Override
@@ -16,13 +16,17 @@ public record DecisionRelevantGameState(Die[] diceInCup, Die[] diceOnTable) {
         if (o == null || getClass() != o.getClass()) return false;
         DecisionRelevantGameState that = (DecisionRelevantGameState) o;
         return Arrays.equals(diceInCup, that.diceInCup) &&
-                Arrays.equals(diceOnTable, that.diceOnTable);
+                Arrays.equals(diceOnTable, that.diceOnTable) &&
+                blastsThisTurn == that.blastsThisTurn &&
+                brainsThisTurn == that.brainsThisTurn;
     }
 
     @Override
     public int hashCode() {
         int result = Arrays.hashCode(diceInCup);
         result = 31 * result + Arrays.hashCode(diceOnTable);
+        result = 31 * result + Integer.hashCode(blastsThisTurn);
+        result = 31 * result + Integer.hashCode(brainsThisTurn);
         return result;
     }
 
